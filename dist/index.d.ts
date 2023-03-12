28e8dd106a79bbed3e073aff4b070ba7c8685bae
cloudhack <allan@openrpa.dk>
2023-03-12 17:06:03 +0100

    expand index

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,3 +1,7 @@
 export { config } from "./config";
 export { protowrap } from "./protowrap";
 export { openiap } from "./openiap";
+export { Ace } from "./proto/ace";
+export { Envelope, User, Role, RefreshToken, FlowService, ErrorResponse } from "./proto/base";
+export { Workitem, WorkItemQueue, WorkitemFile } from "./proto/workitems";
+export { WatchEvent } from "./proto/watch";
