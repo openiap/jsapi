de1e10488f48e4f8ab8d5b84a27d1f120e08e14a
skadefro <allan@openrpa.dk>
2024-05-16 11:00:01 +0200

    wefwe

--- src/openiap.ts
+++ src/openiap.ts
@@ -119,7 +119,7 @@ export class openiap {
     // }
     private async cliOnMessage(client: client, message: Envelope): Promise<any> {
         const [command, BLAHBLAH, reply] = protowrap.unpack(message);
-        if (message.command == "ping") {
+        if (command == "ping") {
             reply.command = "pong";
             return reply;
         } else if (message.command == "refreshtoken") {
