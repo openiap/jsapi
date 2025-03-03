76cb45be3f8210592d7be12d3ea434ea4656c714
skadefro <allan@openrpa.dk>
2025-03-03 09:18:37 +0100

    fix reply for updateworkitemqueue

--- dist/protowrap.js
+++ dist/protowrap.js
@@ -8,7 +8,7 @@ import { BeginStream, CustomCommandResponse, DownloadRequest, DownloadResponse, 
 import { AggregateResponse, CountResponse, DeleteManyResponse, DeleteOneResponse, DropCollectionResponse, GetDocumentVersionResponse, InsertManyResponse, InsertOneResponse, InsertOrUpdateOneResponse, ListCollectionsResponse, CreateCollectionResponse, QueryResponse, UpdateDocumentResponse, UpdateOneResponse } from "./proto/querys.js";
 import { UnWatchResponse, WatchEvent, WatchResponse } from "./proto/watch.js";
 import { QueueEvent, QueueMessageResponse, RegisterExchangeResponse, RegisterQueueResponse, UnRegisterQueueResponse } from "./proto/queues.js";
-import { DeleteWorkitemResponse, PopWorkitemResponse, PushWorkitemResponse, UpdateWorkitemResponse } from "./proto/workitems.js";
+import { DeleteWorkitemResponse, PopWorkitemResponse, PushWorkitemResponse, UpdateWorkItemQueueResponse, UpdateWorkitemResponse } from "./proto/workitems.js";
 export class protowrap {
     static connect(apiurl, onConnected, onDisconnected, onMessage) {
         const result = new client();
@@ -200,6 +200,9 @@ export class protowrap {
                     break;
                 case "deleteworkitemreply":
                     msg = DeleteWorkitemResponse.decode(data);
+                    break;
+                case "updateworkitemqueuereply":
+                    msg = UpdateWorkItemQueueResponse.decode(data);
                     break;
                 case "error":
                     msg = ErrorResponse.decode(data);
